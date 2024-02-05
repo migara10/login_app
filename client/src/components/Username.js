cc0813da370d3f2a5ceb472450bad30ab5895823
unknown <migaraten@gmail.com>
2024-02-05 04:51:57 +0200

    modify validate.js & username.js

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,29 +1,27 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-import avatar from '../assets/profile.png'
-import styles from '../styles/Username.module.css'
-import { Toaster } from 'react-hot-toast'
-import { useFormik } from 'formik'
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
 import { userNameValidate } from "../helper/validate";
 
-
 function Username() {
-
   const formik = useFormik({
     initialValues: {
-      userName: ''
+      userName: "",
     },
     validate: userNameValidate,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit: async values => {
-      console.log(values)
-    }
-  })
-  
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="container mx-auto">
-      <Toaster position="top-right" reverseOrder={false} />
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="flex items-center justify-center h-screen">
         <div className={styles.glass}>
           <div className="flex flex-col items-center title">
@@ -33,16 +31,28 @@ function Username() {
             </span>
           </div>
 
-          <form className="py-1" onSubmit={formik.handleSubmit}> 
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="flex justify-center py-4 profile">
               <img src={avatar} alt="avatar" className={styles.profile_img} />
             </div>
             <div className="flex flex-col items-center textbox">
-              <input {...formik.getFieldProps('userName')} className={styles.textbox} type="text" placeholder="Username" />
-              <button className={styles.btn} type="submit">Let's Go</button>
+              <input
+                {...formik.getFieldProps("userName")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button className={styles.btn} type="submit">
+                Let's Go
+              </button>
             </div>
             <div className="py-4 text-center">
-              <span className="text-green-500" >Not a Member <Link className="text-red-500" to="/register">Register Now</Link></span>
+              <span className="text-green-500">
+                Not a Member{" "}
+                <Link className="text-red-500" to="/register">
+                  Register Now
+                </Link>
+              </span>
             </div>
           </form>
         </div>
